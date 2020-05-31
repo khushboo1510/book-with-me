4c12248cd4f44abfc47daf9c42d1c2e132cbef6e
khushboo1510 <khushboomehta1995@gmail.com>
2020-05-30 20:16:26 -0400

    Navigating to login & register page using if-else

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import Header from'./components/shared/Header';
 import RentalHome from './pages/RentalHome';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
+
+  const renderPages = () => {
+    const { pathname } = window.location;
+    
+    switch(pathname) {
+      case '/':
+        return <RentalHome />;
+      case '/login':
+        return <Login />
+      case '/register':
+        return <Register />
+      default:
+        return <RentalHome />;
+    }
+  }
   return (
     <div>
       <Header />
-      <RentalHome />
+      { renderPages() }
     </div>
   );
 }
---
+++ src/pages/Register.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+class Register extends React.Component {
+
+  render() {
+    return (
+      <h1>Register here!</h1>
+    ) 
+  }
+}
+
+export default Register;
