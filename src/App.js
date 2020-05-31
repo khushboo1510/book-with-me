c5af90fd8614a25de5f0f94affbed4145b9a9706
khushboo1510 <khushboomehta1995@gmail.com>
2020-05-30 20:38:19 -0400

    added react-dom-router to navigate to various pages and putting Link prevents from refreshing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import Header from'./components/shared/Header';
 import RentalHome from './pages/RentalHome';
 import Register from './pages/Register';
@@ -6,24 +11,22 @@ import Login from './pages/Login';
 
 function App() {
 
-  const renderPages = () => {
-    const { pathname } = window.location;
-    
-    switch(pathname) {
-      case '/':
-        return <RentalHome />;
-      case '/login':
-        return <Login />
-      case '/register':
-        return <Register />
-      default:
-        return <RentalHome />;
-    }
-  }
   return (
     <div>
-      <Header />
-      { renderPages() }
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/'>
+            <RentalHome />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/register'>
+            <Register />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
